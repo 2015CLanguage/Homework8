80f57152458022f9b47a3910e76ceb9a87c58bc3
2015CLanguage <buaa_2015_c@163.com>
2015-05-26 15:07:43 +0800

    Create 14151195_张雨生_7_4.c

---
+++ 14151195_张雨生_7_4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void change(int x[3][3],int y[3][3]);
+int main()
+{int a[3][3],b[3][3],i,j;
+	printf("请输入3*3的数组\n");
+	for(i=0;i<3;i++)
+		for(j=0;j<3;j++)
+			scanf("%d",&a[i][j]);
+	printf("\n这个数组是:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+			printf("%3d",a[i][j]);
+		printf("\n");
+	}
+	change(a,b);
+	printf("转置之后:\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++)
+			printf("%3d",b[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
+void change(int x[3][3],int y[3][3])
+{int i,j;
+	for(i=0;i<3;i++)
+		for(j=0;j<3;j++)
+		y[i][j]=x[j][i];
+}
