f48044b080915a5ecbc3fcb8412180955f4a1f6c
2015CLanguage <buaa_2015_c@163.com>
2015-05-26 15:11:19 +0800

    Create 14151166_于琦_8_5.c

---
+++ 14151166_于琦_8_5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+    int n,m=0,o=0,a[100],i;
+	printf("请输入n:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+	    *(a+i)=i+1;
+	}
+    for(i=0;;){
+		if(*(a+i)!=0)
+			o++;
+		if(o==3){
+		*(a+i)=0;
+		o=0;
+		m++;
+		}
+		i++;
+		if(i==n-1)
+			i=0;
+		if(m==n-1)
+			break;
+	}
+	for(i=0;i<n;i++){
+		if(*(a+i)!=0)
+			printf("%d\n",i);
+	}
+}
