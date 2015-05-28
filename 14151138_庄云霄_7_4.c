9779c3cf2b1a42d02e0fcdd4c7df8ed2673a5304
2015CLanguage <buaa_2015_c@163.com>
2015-05-28 20:55:51 +0800

    Create 14151138_庄云霄_7_4.c

---
+++ 14151138_庄云霄_7_4.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void tra(int x[][3])
+{
+	int i,j,temp,flag[3][3]={0};
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if((i!=j)&&(!flag[i][j]))
+			{	temp=x[i][j];
+				x[i][j]=x[j][i];
+				x[j][i]=temp;
+			}
+			flag[i][j]=flag[j][i]=1;
+		}
+	}
+}
+void input(int x[3][3],int n)
+{
+	int i;
+	for(i=0;i<3;i++)
+		scanf("%d",&x[n-1][i]);
+}
+void main()
+{
+	int a[3][3],i,j;
+	printf("请输入3×3数组:\n");
+	printf("请输入第一行：\n");
+	input(a,1);
+	printf("请输入第二行：\n");
+	input(a,2);
+	printf("请输入第三行：\n");
+	input(a,3);
+	tra(a);
+	printf("转置后为：\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+			printf("%5d",a[i][j]);
+		printf("\n");
+	}
+}
