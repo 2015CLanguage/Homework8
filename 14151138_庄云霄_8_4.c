48944ff5b504530608e890939b2166c3738ee413
2015CLanguage <buaa_2015_c@163.com>
2015-05-28 23:16:28 +0800

    Update 14151138_庄云霄_8_4.c

--- 14151138_庄云霄_8_4.c
+++ 14151138_庄云霄_8_4.c
@@ -1,39 +1,60 @@
-#include <stdio.h>
-int main()
-{
-	void haha(int a[],int m);
-	int a[10],b,m,i;
-	printf("请输入m值：");
+//整体设计思路：定义一个数组list1，接收终端输入想要输入的数字个数，然后通过另外一个数组list2将list1里面的数字
+//复制两次，接受终端想要转移几位数字，如输入list1:1,2,3,4,5,6,7,8；在list2中即产生1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8
+//然后从想要转换的数开始进行遍历，将list1转化成想要实现的那个列表
+
+#include<stdio.h>
+int main(){
+	
+	void input(int *list1,int n);	
+	void move(int *list1,int m,int n);
+	void output(int *list1,int n);
+	int n,m,list1[10];
+	printf("please input how many number will you input(<11):\n");
+	scanf("%d",&n);
+	printf("please input %d numbers:\n",n);
+	input(list1,n);
+	printf("how many places do you want to move?\n");
 	scanf("%d",&m);
-	for(i=0;i<10;i++)
-	{
-		scanf("%d",&a[i]);
+	move(list1,m,n);
+	output(list1,n);
+
+
+
+}
+void output(int *list1,int n){
+	printf("now they are:\n");
+	int *p;
+	for(p=list1;p<list1+n;p++){
+		printf("%d",*p);
 	}
-	tiao(a,m);
-	for(i=0;i<10;i++)
-	{
-		printf("%d\n",*(a+i));
+} 
+void move(int *list1,int m,int n){
+	int list2[20]={0},*p,*q;
+	q=list2;
+	for(p=list1;p<list1+n;p++,q++){
+		*q=*p;
 	}
-	return 0;
+	for(p=list1,q=list2+n;p<list1+n;p++,q++){
+		*q=*p;
+	}
+	
+	for(q=list2+n-m,p=list1;q<list2+n+n-m;q++,p++){
+		printf("%d",*q);
+		*p=*q;
+	}
+	
+	
+	
+	
+	
 }
 
-void haha(int a[],int m)
-{
-	int b[10],i;
-	for(i=0;i<10;i++)
-	{
-		if(i<m)
-		{
-		*(b+i)=*(a+9-m+1+i);
-	}
-	else
-	{
-		*(b+i)=*(a+i-m);
-	}
-	}
-	for(i=0;i<10;i++)
-	{
-		*(a+i)=*(b+i);
+
+
+void input (int *list1,int n){
+	int *p;
+	for(p=list1;p<list1+n;p++){
+		scanf("%d",p);
 	}
 
 }
