74da5f82fbc836fe8382f52e3f4f7429fc1544c5
2015CLanguage <buaa_2015_c@163.com>
2015-05-29 12:20:59 +0800

    Create 14151172_程涛_8_4.c

---
+++ 14151172_程涛_8_4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void exchange(int *a, int n, int m){
+  int i;
+	for(i=0;i<n-m;i++){
+	    *(a+n+i)=*(a+i);
+	}
+}
+void main()
+{
+  int i,n,m,a[100]={0};
+	printf("请输入n:");
+	scanf("%d",&n);
+  printf("请输入m:");
+	scanf("%d",&m);
+	for(i=0;i<n;i++){
+	    scanf("%d",a+i);
+	}
+	exchange(a,n,m);
+	for(i=n-m;i<2*(n-m)+m;i++){
+	    printf("%d",*(a+i));
+	}
+	printf("\n");
+}
