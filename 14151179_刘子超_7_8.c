a2cb8530b05fc8d4c1e762fceace99e703b0febb
2015CLanguage <buaa_2015_c@163.com>
2015-05-30 23:53:01 +0800

    Create 14151179_刘子超_7_8.c

---
+++ 14151179_刘子超_7_8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void change(char a[8]);
+void main()
+{
+    char a[8]={0};
+    int i;
+    printf("number：\n");
+    gets(a);
+    change(a);
+    puts(a);
+}
+void change(char a[8])
+{ 
+    int j; 
+    for(j=3;j>0;j--)
+    { 
+        a[j*i]=aji]; 
+        a[2*j-1]=' '; 
+    } 
+} 
