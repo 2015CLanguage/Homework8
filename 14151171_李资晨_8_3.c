1010b221e3198b94dfc4cfefb50983d8a2521323
2015CLanguage <buaa_2015_c@163.com>
2015-05-31 00:01:42 +0800

    Create 14151171_李资晨_8_3.c

---
+++ 14151171_李资晨_8_3.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int num[10]={0};
+void ru(int *);
+void chu(int *);
+int main()
+{
+
+	ru(num);
+    int *min,*max,*m,*n,t,h;
+	max=min=num;
+	for(m=num+1;m<num+10;m++)
+	{
+		if(*m>*max)
+		{
+			max=m;
+		}
+	}
+	t=num[9];
+	num[9]=*max;
+	*max=t;
+    for(n=num+1;n<num+10;n++)
+	{
+		if(*n<*min)
+		{
+			min=n;
+		}
+	}
+	h=num[0];
+	num[0]=*min;
+	num[0]=h;
+	chu (num);
+}
+void ru(int *num)
+{
+	int *s;
+	for(s=num;s<num+10;s++)
+	{
+		scanf("%d,",s);
+	}
+	printf("\n");
+}
+void chu(int *num)
+{
+	int *f;
+	for(f=num;f<num+10;f++)
+	{
+		printf("%d\n",*f);
+
+	}
+}
