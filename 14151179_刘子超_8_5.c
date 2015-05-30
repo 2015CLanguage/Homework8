d7c18d77fac0c7e74172c036d9b2286b7e800f86
2015CLanguage <buaa_2015_c@163.com>
2015-05-30 23:45:10 +0800

    Create 14151179_刘子超_8_5.c

---
+++ 14151179_刘子超_8_5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int out(int a[20],int);
+int a[20];
+int main()
+{
+	int i,n,p;
+	printf("n is");
+	scanf("%d",&n);
+	printf("\n");
+	for(i=0;i<n;i++)
+		a[i]=i;
+	p=out(a,n);
+	printf("he is %d",p);
+	return 0;
+}
+int out(int a[20],int n)
+{
+	int k=0,s,*p=a,j=0,t=0;
+	while(t<n-1)
+	{
+		if(*(p+j)!=0)k++;
+		if(k==3)
+		{
+			k=0;t++;*(p+j)=0;
+		}
+		j++;
+		if(j==n)j==0;
+	}
+	while(*p==0)p++;
+	s=*p;
+	return s;
+}
