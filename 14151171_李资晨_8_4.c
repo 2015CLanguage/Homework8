adc26ee8f8ef8d2058a479ecb493a8485760df57
2015CLanguage <buaa_2015_c@163.com>
2015-05-31 00:04:24 +0800

    Create 14151171_李资晨_8_4.c

---
+++ 14151171_李资晨_8_4.c
@@ -0,0 +1,25 @@
+#include"stdio.h"
+void change(int a[20],int,int);
+int a[20];
+int main()
+{
+	int i,j,n,m;
+	printf("n is");
+	scanf("%d",&n);
+	printf("\n");
+	printf("m is");
+	scanf("%d",&m);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	change(a,n,m);
+	printf("now:");
+    for(j=0;j<n;j++)
+		printf("%d",a[j]);
+}
+void change(int a[20],int n,int m)
+{
+	int b[20],k,s,t;
+	for(k=0;k<n-m;k++)b[k]=a[n-m+k];
+	for(s=0;s<n-m;s++)b[n-m+s]=a[s];
+    for(t=0;t<n;t++)a[t]=b[t];
+}
