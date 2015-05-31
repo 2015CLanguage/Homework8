7fce475d87fd2cdad216b9be2183d3bfefb06a9c
2015CLanguage <buaa_2015_c@163.com>
2015-05-31 22:25:49 +0800

    Create 14151168_张辰乙_7_8.c

---
+++ 14151168_张辰乙_7_8.c
@@ -0,0 +1,27 @@
+ # include <stdio.h> 
+ #include <math.h> 
+ void split(int x); 
+ 
+ 
+ 
+ 
+ int main() 
+ { 
+     int x; 
+     printf("请输入四位整数:"); 
+     scanf("%d",&x); 
+     split(x); 
+     return 0; 
+ } 
+ 
+ 
+ 
+ 
+ void split(int x) 
+ { 
+     int m,n,p; 
+     m=x%1000; 
+     n=m%100; 
+     p=n%10; 
+     printf("%d %d %d %d",(x-m)/1000,(m-n)/100,(n-p)/10,p); 
+ } 
