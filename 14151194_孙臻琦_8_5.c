dcbbeb4872ff6ca75843518d10f3b61356bb39d3
2015CLanguage <buaa_2015_c@163.com>
2015-06-01 20:10:12 +0800

    Create 14151194_孙臻琦_8_5.c

---
+++ 14151194_孙臻琦_8_5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,k,m,n,num[50],*p;
+	printf("请输入人数n:\n");
+	scanf("%d",&n);
+	p=num;
+	for(i=0;i<n;i++)
+		*(p+i)=i+1;
+	i=0;
+	k=0;
+	m=0;
+	while(m<n-1)
+	{
+		if(*(p+i)!=0) k++;
+		if(k==3)
+		{
+			*(p+i)=0;
+			k=0;
+			m++;
+		}
+		i++;
+		if(i==n)i=0;
+	}
+	while(*p==0) p++;
+	printf("最后一个是第%d个人\n",*p);
+	return 0;
+}
