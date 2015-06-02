ccb4cd5f247fa693c68ae2c66f8bb6456ee62549
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 12:48:19 +0800

    Create 14151176_唐飞宇_7_8.c

---
+++ 14151176_唐飞宇_7_8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void c(char s[]);
+
+
+int main()
+{
+	char s[80];
+	printf("请输入四个数字:\n");//接受输入
+	scanf("%s",s);
+	c(s);//调用字符函数
+	return 0;
+}
+
+void c(char s[])//定义字符函数
+{
+	int i;
+	for(i=strlen(s);i>0;i--)
+	{
+		s[2*i]=s[i];
+		s[2*i-1]=' ';
+	}
+	printf("数字字符是:\n%s\n",s);
+}
