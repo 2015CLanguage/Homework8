c2a0a94a733fe51640948c7b1a70fbffee3cf97a
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 12:46:12 +0800

    Create 14151176_唐飞宇_8_4.c

---
+++ 14151176_唐飞宇_8_4.c
@@ -0,0 +1,33 @@
+# include<stdio.h>
+void move(int [30],int n,int m);
+void move(int number[30],int n,int m)
+{
+	int *p,numberend;
+	numberend=*(number+n-1); 
+	for(p=number+n-1;p>number;p--)
+		*p=*(p-1);
+	*number=numberend;
+	m--;
+	if(m>0)
+		move(number,n,m);
+}
+
+
+int main(void)
+
+{
+	int number[30],n,m,i;
+	printf("输入整数的个数n:\n");
+	scanf("%d",&n);
+	printf("输入这%d个整数：\n",n);
+	for(i=0;i<n;i++)
+	scanf("%d",&number[i]);
+	printf("输入移动的位数：\n");
+	scanf("%d",&m);
+	move(number,n,m);
+	printf("处理后的数为：\n");
+	for(i=0;i<n;i++)
+	printf("%d",number[i]);
+	printf("\n");
+	return 0;
+}
